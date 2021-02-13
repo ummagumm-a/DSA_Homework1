23bb1d0c8cf835800d8470562198137b1f538bae
ummagumm_a <siniy.vyacheslav@gmail.com>
2021-02-13 16:23:48 +0300

    beginning of the work

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * This is the first homework for DSA course in Innopolis University.
+ *
+ * Author - Sinii Viacheslav
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
